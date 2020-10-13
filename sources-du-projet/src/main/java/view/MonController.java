7d1f899aabae03558d9bc7113abd28855bf735fd
Alexis Duhem <duhemalexis048@gmail.com>
2020-10-14 00:17:18 +0200

    ListView ok

--- sources-du-projet/src/main/java/view/MonController.java
+++ sources-du-projet/src/main/java/view/MonController.java
@@ -1,27 +1,14 @@
 package view;
 
-
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
-
 public class MonController {
 	@FXML ListView <String > list;
 
 	public void initialize() {
-		Directory dir = null;
-		try{
-			dir = new Directory("C:\\Users\\duhem\\Desktop\\projetmode-h1\\sources-du-projet\\src\\main\\resources\\fichiers");
-			list = new ListView<String>();
-			list.getItems().addAll(dir.getListOfFiles());
-			list.notify();
-		}catch (Exception e) {
-			// TODO: handle exception
-			System.out.println(e.getMessage());
-		}
-	}
-	public void afficher() {
-		list.getItems().add("coucou");
-		System.out.println(list.getItems());
+		Directory dir = new Directory("src/main/resources/fichiers/");
+		list.getItems().addAll(dir.getListOfFiles());
+		list.refresh();
 	}
 
 }
