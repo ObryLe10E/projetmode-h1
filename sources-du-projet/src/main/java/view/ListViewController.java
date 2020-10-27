2f1001ee84dafac6a8e60ea064a452a972fc59b5
Alexis Duhem <duhemalexis048@gmail.com>
2020-10-27 01:17:27 +0100

    J'ai compris des trucs en css et en cellule de listview

--- sources-du-projet/src/main/java/view/ListViewController.java
+++ sources-du-projet/src/main/java/view/ListViewController.java
@@ -2,26 +2,53 @@ package view;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
-import javafx.util.Callback;
 import modele.Face;
 import modele.Point;
 import modele.Reader;
 import modele.Repere;
 
 public class ListViewController {
+    class Cell extends ListCell<File>{
+    	HBox hbox = new  HBox();
+    	Button button = new Button("ok");
+    	Image img = new Image("https://img.icons8.com/color/72/file.png");
+    	ImageView imgv = new ImageView(img);
+    
+    	
+    	
+    	public Cell() {
+    		super();
+    		button.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;");
+    		imgv.maxWidth(1);
+    		imgv.maxHeight(1);
+    		hbox.getChildren().addAll(imgv,button);
+    		this.setStyle("-fx-background-color: transparent");
+    	}
+    	public void updateItem(File item, boolean empty) {
+    		super.updateItem(item, empty);
+    		setText(null);
+    		setGraphic(null);
+    		if(item != null && !empty) {
+        		this.setStyle("-fx-background-color: darkslateblue; -fx-background-radius : 20px; -fx-border-color:ffffff; -fx-border-radius : 20px");
+    			setText(item.getName());
+    			setGraphic(hbox);
+    		}
+    	}
+    }
 
     @FXML
     ListView<File> list;
@@ -34,30 +61,7 @@ public class ListViewController {
         list.getItems().addAll(dir.getListOfFiles());
         list.refresh();
         list.getSelectionModel().getSelectedItems().addListener(new FileListChangeListener());
-        list.setCellFactory(new CellFact());
-    }
-
-    class CellFact extends ListCell<File> implements Serializable, Callback<ListView<File>, ListCell<File>> {
-        private static final long serialVersionUID = 1L;
-
-        @Override
-        public void updateItem(File item, boolean b) {
-
-            super.updateItem(item, b);
-            if (item != null && !b) {
-                Canvas c = new Canvas(400, 20);
-                GraphicsContext gc = c.getGraphicsContext2D();
-                gc.fillText(item.getName(), 25, 16);
-                setGraphic(c);
-            } else {
-                setGraphic(null);
-            }
-        }
-
-        @Override
-        public ListCell<File> call(ListView<File> param) {
-            return new CellFact();
-        }
+        list.setCellFactory(param-> new Cell());
     }
 
     class FileListChangeListener implements ListChangeListener<File> {
